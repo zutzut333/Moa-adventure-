4d80b29161d9fc27603469407c98dda70adbebad
NathanaelCollaud <61788141+NathanaelCollaud@users.noreply.github.com>
2021-12-09 09:16:03 +0100

    Préparation du projet
    Création du projet
    Architecture "content"
    quelques classes

---
+++ MoaAdventure/Creature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoaAdventure
+{
+    class Creature
+    {
+        private string _name;
+        private int _speed;
+
+        void Attack() 
+        { }
+        void Move(string direction) { }
+    }
+}
