087d3cddc9584697edfc4e8e8300377d3bca960d
zutzut333 <61789885+zutzut333@users.noreply.github.com>
2021-12-17 09:11:14 +0100

    .

---
+++ MoaAdventure/LevelLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace MazeRunner
+{
+    public class LevelLoader
+    {
+        private Map _map;
+
+        public LevelLoader(string levelFilename)
+        {
+            ParseContent(levelFilename);
+        }
+
+        public Map Map
+        {
+            get { return _map; }
+        }
+
+        private void ParseContent(string levelFilename)
+        {
+            string[] fileLines = File.ReadAllLines(levelFilename);
+            int width = fileLines[0].Length;
+            int height = fileLines.Length;
+
+            int[,] tileMap = new int[width, height];
+            for (int line = 0; line < height; line++)
+            {
+                string currentFileLine = fileLines[line];
+                for (int column = 0; column < width; column++)
+                {
+                    char item = currentFileLine[column];
+                    if (item >= 'A' && item <= 'Z')
+                    {
+                        tileMap[column, line] = item - '@';
+                    }
+                }
+            }
+
+            _map = new Map(width, height, tileMap);
+        }
+    }
+}
---
+++ MoaAdventure/Map.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MazeRunner
+{
+    public class Map
+    {
+        private int _width;
+        private int _height;
+        private int[,] _map;
+
+        public Map(int width, int height, int[,] tileIds)
+        {
+            _width = width;
+            _height = height;
+            _map = tileIds;
+        }
+
+        public int Height
+        {
+            get { return _height; }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+        }
+
+        public int TileIdAt(int x, int y)
+        {
+            return _map[x, y];
+        }
+
+        public bool HasWallAt(int x, int y)
+        {
+            return _map[x, y] != 0;
+        }
+    }
+}
