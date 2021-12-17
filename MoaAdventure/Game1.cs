b6a743d4c1816b99bbe49f7d4395d9fd67c39169
NathanaelCollaud <61788141+NathanaelCollaud@users.noreply.github.com>
2021-12-17 16:04:30 +0100

    boom

--- MoaAdventure/Game1.cs
+++ MoaAdventure/Game1.cs
@@ -13,7 +13,12 @@ namespace MoaAdventure
         //World world;
         //Hero hero;
         Vector2 heroPosition;
+        Texture2D heroTextureFace;
+        Texture2D heroTextureGauche;
+        Texture2D heroTextureDroite;
+        Texture2D heroTextureBack;
         Texture2D heroTexture;
+
         float heroSpeed;
 
         public Game1()
@@ -38,6 +43,10 @@ namespace MoaAdventure
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             heroTexture = Content.Load<Texture2D>("obj/Windows/Content/Perso face");
+            heroTextureGauche = Content.Load<Texture2D>("obj/Windows/Content/Perso gauche");
+            heroTextureBack = Content.Load<Texture2D>("obj/Windows/Content/Perso haut");
+            heroTextureDroite = Content.Load<Texture2D>("obj/Windows/Content/Perso droite");
+            heroTextureFace = Content.Load<Texture2D>("obj/Windows/Content/Perso face");
             // TODO: use this.Content to load your game content here
         }
 
@@ -51,17 +60,30 @@ namespace MoaAdventure
 
 
             if (kstate.IsKeyDown(Keys.Down))
+            {
                 heroPosition.Y += heroSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                heroTexture = heroTextureFace;
+            }
+                
 
             if (kstate.IsKeyDown(Keys.Left))
+            {
                 heroPosition.X -= heroSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Right))
+                heroTexture = heroTextureGauche;
+            }
+               
+            if (kstate.IsKeyDown(Keys.Right)) 
+            {
                 heroPosition.X += heroSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Up))
+                heroTexture = heroTextureDroite;
+            }
+                
+            if (kstate.IsKeyDown(Keys.Up)) 
+            {
                 heroPosition.Y -= heroSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
+                heroTexture = heroTextureBack;
+            }
+                
             if (heroPosition.X > _graphics.PreferredBackBufferWidth - heroTexture.Width / 2)
                 heroPosition.X = _graphics.PreferredBackBufferWidth - heroTexture.Width / 2;
             else if (heroPosition.X < heroTexture.Width / 2)
