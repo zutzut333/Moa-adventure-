04879e76ad6190b289c97298241cf8e2b154e5a1
zutzut333 <61789885+zutzut333@users.noreply.github.com>
2021-12-23 10:36:02 +0100

    background

--- MoaAdventure/Game1.cs
+++ MoaAdventure/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using  System.Collections.Generic;
 
 namespace MoaAdventure
 {
@@ -8,6 +9,11 @@ namespace MoaAdventure
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        public int ActualLevel = 1;
+        private Texture2D mainTexture;
+        
+        private List<Texture2D> _backgroundsList;
+
 
         public Game1()
         {
@@ -18,14 +24,23 @@ namespace MoaAdventure
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+
+            _backgroundsList = new List<Texture2D>();
+
+            _backgroundsList.Add(Content.Load<Texture2D>("Sprites/background wood"));
+            _backgroundsList.Add(Content.Load<Texture2D>("Sprites/background snow"));
+            _backgroundsList.Add(Content.Load<Texture2D>("Sprites/background_champs"));
+            _backgroundsList.Add(Content.Load<Texture2D>("Sprites/background_fortress"));
+
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -37,14 +52,34 @@ namespace MoaAdventure
 
             // TODO: Add your update logic here
 
+            switch (ActualLevel)
+            {
+                case 1:
+                    mainTexture = _backgroundsList[0];
+                    break;
+                case 2:
+                    mainTexture = _backgroundsList[1];
+                    break;
+                case 3:
+                    mainTexture = _backgroundsList[2];
+                    break;
+                case 4:
+                    mainTexture = _backgroundsList[3];
+                    break;
+            }
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(mainTexture, new Vector2(0, 0), null, Color.White);
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
---
+++ MoaAdventure/Input.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MoaAdventure
+{
+    public class Input
+    {
+        public static bool ExitRequested
+        {
+            get { return GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape); }
+        }
+
+        public static bool PlayerDown
+        {
+            get { return Keyboard.GetState().IsKeyDown(Keys.Down); }
+        }
+
+        public static bool PlayerUp
+        {
+            get { return Keyboard.GetState().IsKeyDown(Keys.Up); }
+        }
+
+        public static bool PlayerLeft
+        {
+            get { return Keyboard.GetState().IsKeyDown(Keys.Left); }
+        }
+
+        public static bool PlayerRight
+        {
+            get { return Keyboard.GetState().IsKeyDown(Keys.Right); }
+        }
+    }
+}
