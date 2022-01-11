972fb40c15afbc2954c3934d7e27882697a8c1c8
NathanaelCollaud <61788141+NathanaelCollaud@users.noreply.github.com>
2022-01-11 15:07:08 +0100

    test

---
+++ test/testMG/core/ObjectClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace testMG.core
+{
+    class ObjectClass
+    {
+        public Vector2 Position;
+        public Texture2D Texture;
+    }
+}
