3a1a74b0681997b5eeaf3a90abb8115c7b2792fb
zutzut333 <61789885+zutzut333@users.noreply.github.com>
2022-01-20 15:38:58 +0100

    .

--- MoaAdventure/Game1.cs
+++ MoaAdventure/Game1.cs
@@ -17,7 +17,7 @@ namespace MoaAdventure
 
         private List<string> direction;
 
-        public int ActualLevel = 3;
+        public int ActualLevel = 1;
         private Texture2D _mainTexture;
         private Map _actualMap;
         public bool isDoorPassed = true;
